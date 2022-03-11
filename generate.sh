e23340b7e257f0ffb348ddd6449518abc9c38d58
meinczinger <meinczinger@gmail.com>
2022-03-11 08:52:52 +0100

    refactor

--- generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# protoc --go-grpc_out=. greet/greetpb/greet.proto 
-protoc --proto_path=./greet/greetpb --go_out=./greet/greetpb/go --go_opt=paths=source_relative \
-    --go-grpc_out=./greet/greetpb/go --go-grpc_opt=paths=source_relative \
-    greet.proto 
--- generate_python.sh
+++
@@ -1,2 +0,0 @@
-python -m grpc_tools.protoc -Igreet/greetpb --python_out=greet/greetpb/python  \
---grpc_python_out=greet/greetpb/python greet/greetpb/greet.proto
